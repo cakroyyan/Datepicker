65b9f3f4853e0d727c6edfe1de69a43ab77e934e
Royyan <cakroyyan@gmail.com>
2016-10-25 12:45:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Menampilkan Datepicker di Modal Bootstrap</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <style>
+        .datepicker {
+            z-index: 1151;
+        }
+    </style>
+    <script>
+        $(function() {
+            $("#tanggal").datepicker({
+                format: 'yyyy/dd/mm'
+            });
+        });
+    </script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <label>Tanggal</label>
+        <input type="text" id="tanggal">
+    </div>
+
+</body>
+
+</html>
